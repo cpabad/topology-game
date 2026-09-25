6b53972ea9612856315719035d0ed209480a2eaa
cpabad <ca132731@gmail.com>
2026-09-25 13:52:14 -0500

    Import Set.Basic and Set.Lattice in Metadata

--- Game/Metadata.lean
+++ Game/Metadata.lean
@@ -1,5 +1,7 @@
 import GameServer
 import Mathlib.Tactic.Common
+import Mathlib.Data.Set.Basic
+import Mathlib.Data.Set.Lattice
 
 /-! Use this file to add things that should be available in all levels.
 
